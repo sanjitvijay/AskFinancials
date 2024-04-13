2a8ed8854bffe61a092add65e0c55956e0a20f66
soham <sohamsalunke10@gmail.com>
2024-04-13 14:05:04 -0400

    added text streaming

--- src/App.js
+++ src/App.js
@@ -1,39 +1,46 @@
 import { useState, useEffect } from "react";
 import genAI from "./googleClient";
 function App() {
-  const [story, setStory] = useState('')
-  const [prompt, setPrompt]= useState('')
-  const [loading, setLoading] = useState(false)
+  const [story, setStory] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [loading, setLoading] = useState(false);
   const onChange = (e) => {
-    setPrompt(e.target.value)
-  }
+    setPrompt(e.target.value);
+  };
+  const STORY_CHUNK_SIZE = 10; // Update story every 100 characters
+
   const run = async (e) => {
-    e.preventDefault()
-    const model = genAI.getGenerativeModel({model: "gemini-pro"})
-
-    setLoading(true)
-    const result = await model.generateContent(prompt)
-    const response = await result.response
-    setLoading(false)
-
-    const text = response.text()
-    setStory(text)
-  }
+    setStory("");
+    e.preventDefault();
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    let text;
+    const result = await model.generateContentStream([prompt]);
+    for await (const chunk of result.stream) {
+      text += chunk.text();
+      if (text.length > STORY_CHUNK_SIZE) {
+        while (text.length != 0) {
+          const slicedText = text.slice(0, 10);
+          setStory(prevStory => prevStory + slicedText);
+          text = text.slice(10);
+        }
+      }
+    }
+  };
   return (
     <div className="p-5">
       <div className="flex justify-center">
         <form>
-          <input 
+          <input
             className="input input-bordered input-neutral"
             onChange={onChange}
-          >
-
-          </input>
-          <button type='submit' className="btn btn-primary ml-5" onClick={run}>Run</button>
+          ></input>
+          <button type="submit" className="btn btn-primary ml-5" onClick={run}>
+            Run
+          </button>
         </form>
       </div>
-      
-      <p>{loading ? 'loading' : story}</p>
+
+      <p>{loading ? "loading" : story}</p>
     </div>
   );
 }
